78d87f196d635fc64db4a80a4bbbe8a16bde7684
S123Ab <doc1sid@hotmail.com>
2023-02-10 14:05:05 +0000

    Created books grid components that gets list of books from API and displays on Books page

---
+++ pages/api/books.js
@@ -0,0 +1,22 @@
+export default function handler(req, res) {
+    const Books = [
+    {        
+
+        title: "Why You?",
+        author: "James Reed?",
+
+    },
+    {
+        title:  "Parenting a child with Autism",
+        author: "Richard Bass",
+
+    },
+    {
+        title: "Emotional Intelligence",
+        author: "Daniel Goleman",
+    }
+
+    ];
+
+    res.json(Books);
+}
