03296574182b7773ec80e0b82b3dbf4bf54a9b21
S123Ab <doc1sid@hotmail.com>
2023-02-10 14:07:57 +0000

    Added some examples of components from Nextjs including <images> and <Links>

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,33 @@
+import Link from "next/link"
+import { useRouter } from "next/router";
+import { useState } from "react";
+
 export default function Homepage() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    alert("you clicked the button!");
+
+    //  make API call
+    router.push("/books");
+    // navigate to book page
+  }
   return (
-    <div className="w-full py-10 bg-blue-100">Hello</div>
+    <div className="w-full py-10 bg-blue-100">Hello
+
+    <button 
+      className="bg-blue-500 px-2 py-1 rounded"
+      onClick= {handleClick}
+    >
+
+      click me!!!
+    </button>
+
+    <div>
+      <Link href= "/books">
+        Go to books page 
+      </Link>
+    </div>
+  </div>
   )
 }
