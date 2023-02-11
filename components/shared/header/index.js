8d95a64609091a8f975b9ab20a6c1e28a12d4789
S123Ab <doc1sid@hotmail.com>
2023-02-11 16:32:02 +0000

    Added header

---
+++ components/shared/header/index.js
@@ -0,0 +1,8 @@
+export default function Header({ name }) {
+    return (
+        <div className="bg-blue-100 w-full max-w-6xl mx-auto py-4 px-4">
+        <h1 className="font-bold text-4xl">{name} page</h1>
+        
+    </div>  
+    )
+}
