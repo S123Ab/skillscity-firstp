05ead2d0056ec5188faf1d6cb30ab28ccad52b43
S123Ab <doc1sid@hotmail.com>
2023-02-11 16:31:07 +0000

    Added styling

--- pages/books.js
+++ pages/books.js
@@ -1,20 +1,22 @@
 import BooksGrid from "@/components/books/books-grid";
+import Footer from "@/components/shared/footer";
+import Header from "@/components/shared/header";
 import Link from "next/link";
 
 
 export default function BooksPage() {
     return (
-        <div className="bg-green-100">
-            Books page
-
-
-            <BooksGrid/>
-
-            <div>
-                <Link href="/">
-                    Go to Homepage
-                </Link>
+        <div className="w-full">
+            <Header name="Books"/>
+                
+            
+            <div className="w-full max-w-6xl mx-auto mt-8">
+                <BooksGrid/>
             </div>
+
+            <Footer title="Home Page" href="/" />
+            
         </div>
     )
+
 }
