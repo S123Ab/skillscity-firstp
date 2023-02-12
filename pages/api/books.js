9ade156344126117354504a2984e3421f4fe1d4d
S123Ab <doc1sid@hotmail.com>
2023-02-12 15:29:50 +0000

    Added imgUrl to books.js

--- pages/api/books.js
+++ pages/api/books.js
@@ -1,22 +1,26 @@
-export default function handler(req, res) {
+export default function handler(req, res) 
+{
+
     const Books = [
     {        
 
         title: "Why You?",
         author: "James Reed",
+        imgUrl: "/assets/book1.jpg"
 
     },
     {
         title:  "Parenting a child with Autism",
         author: "Richard Bass",
+        imgUrl: "/assets/book2.jpg"
+
 
     },
     {
         title: "Emotional Intelligence",
         author: "Daniel Goleman",
-    }
-
-    ];
+        imgUrl: "/assets/book3.jpg"
+    }];
 
     res.json(Books);
 }
