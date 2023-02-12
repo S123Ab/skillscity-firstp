298d0261423642ef666a1ba70d7f03403cb0cb1b
S123Ab <doc1sid@hotmail.com>
2023-02-12 13:52:12 +0000

    Removed question mark from the Author name.

--- pages/books.js
+++ pages/books.js
@@ -1,4 +1,5 @@
 import BooksGrid from "@/components/books/books-grid";
+import Content from "@/components/shared/content";
 import Footer from "@/components/shared/footer";
 import Header from "@/components/shared/header";
 import Link from "next/link";
@@ -8,11 +9,16 @@ export default function BooksPage() {
     return (
         <div className="w-full">
             <Header name="Books"/>
+
+            <Content>
+                <div className="w-full max-w-6xl mx-auto mt-8">
+                    <BooksGrid/>
+                </div>
+
+            </Content>
                 
             
-            <div className="w-full max-w-6xl mx-auto mt-8">
-                <BooksGrid/>
-            </div>
+            
 
             <Footer title="Home Page" href="/" />
             
