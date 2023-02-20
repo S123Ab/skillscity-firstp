e80c49047ad4524abdd9be03217c8365a4ca6a4f
S123Ab <doc1sid@hotmail.com>
2023-02-20 22:47:55 +0000

    Added ID

--- pages/api/books.js
+++ pages/api/books.js
@@ -3,13 +3,14 @@ export default function handler(req, res)
 
     const Books = [
     {        
-
+        Id:"1",
         title: "Why You?",
         author: "James Reed",
         imgUrl: "/assets/book1.jpg"
 
     },
     {
+        Id:"2",
         title:  "Parenting a child with Autism",
         author: "Richard Bass",
         imgUrl: "/assets/book2.jpg"
@@ -17,6 +18,7 @@ export default function handler(req, res)
 
     },
     {
+        Id:"3",
         title: "Emotional Intelligence",
         author: "Daniel Goleman",
         imgUrl: "/assets/book3.jpg"
