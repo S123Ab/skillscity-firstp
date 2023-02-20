e4b61aa9e885dfae1738657edf28e8fdfaf0bc22
S123Ab <doc1sid@hotmail.com>
2023-02-20 22:47:28 +0000

    Added LocationsPage function

---
+++ pages/locations.js
@@ -0,0 +1,28 @@
+import LocationsGrid from "@/components/locations/locations-grid";
+import Content from "@/components/shared/content";
+import Footer from "@/components/shared/footer";
+import Header from "@/components/shared/header";
+import Link from "next/link";
+
+
+export default function LocationsPage() {
+    return (
+        <div className="w-full">
+            <Header name="Locations"/>
+
+            <Content>
+                <div className="w-full max-w-6xl mx-auto mt-8">
+                    <LocationsGrid/>
+                </div>
+
+            </Content>
+                
+            
+            
+
+            <Footer title="Home Page" href="/" />
+            
+        </div>
+    )
+
+}
