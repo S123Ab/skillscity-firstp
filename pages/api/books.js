8369d2d0dc601953a3754123fb41496666fbb822
S123Ab <doc1sid@hotmail.com>
2023-02-24 12:01:44 +0000

    gets books from database

--- pages/api/books.js
+++ pages/api/books.js
@@ -1,31 +1,11 @@
 import sql from "@/utils/postgres";
 
-export default function handler(req, res) {
-    
-        
+export default async function handler(req, res) {
+        const books = await sql`
+            select * from books
 
-    const Books = [
-    {        
-        Id:"1",
-        title: "Why You?",
-        author: "James Reed",
-        imgUrl: "/assets/book1.jpg"
+            `;
 
-    },
-    {
-        Id:"2",
-        title:  "Parenting a child with Autism",
-        author: "Richard Bass",
-        imgUrl: "/assets/book2.jpg"
-
-
-    },
-    {
-        Id:"3",
-        title: "Emotional Intelligence",
-        author: "Daniel Goleman",
-        imgUrl: "/assets/book3.jpg"
-    }];
-
-    res.json(Books);
+            
+    res.json(books);
 }
