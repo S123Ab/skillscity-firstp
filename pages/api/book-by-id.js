371b6f8152151a59e082e6deefa050292a9b128c
S123Ab <doc1sid@hotmail.com>
2023-02-24 16:21:41 +0000

    Get single book from database

--- pages/api/book-by-id.js
+++ pages/api/book-by-id.js
@@ -1,45 +1,23 @@
-export default function handler(req, res) 
-{
+import sql from "@/utils/postgres";
+
+export default async function handler(req, res) {
     const id = req.query.id;
+
+    const books = await sql`
+        select * from books
+        where id = ${id}
+    
+    `;
+
+    if (books.length < 1) {
+        res.status(404).json({ message: "Book not found"});
+        return;
+    }
    
 
-    const Books = [
-    {        
-        Id:"1",
-        title: "Why You?",
-        author: "James Reed",
-        imgUrl: "/assets/book1.jpg",
-        linkToPurchase: "https://amzn.eu/d/eHlVNfm"
-
-    },
-    {
-        Id:"2",
-        title:  "Parenting a child with Autism",
-        author: "Richard Bass",
-        imgUrl: "/assets/book2.jpg",
-        linkToPurchase: "https://amzn.eu/d/9lrvMCK"
+    const foundBook = books[0];
 
 
-    },
-    {
-        Id:"3",
-        title: "Emotional Intelligence",
-        author: "Daniel Goleman",
-        imgUrl: "/assets/book3.jpg",
-        linkToPurchase: "https://amzn.eu/d/jgCllRp"
 
-
-    
-    }];
-
-    const foundBook = Books.find((book) => {
-        if (book.Id === id) {
-            return true;
-        }
-        else{
-            return false;
-        }
-    })
-
-    res.json({book: foundBook});
+    res.json({ book: foundBook });
 }
